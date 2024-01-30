5b8eebc088333aa4d216950577459e76bf644fd8
Panchezzz <154076820+Panchezzz@users.noreply.github.com>
2024-01-30 21:15:05 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1706630709,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/block-sheet0.png",
+		"images/player-sheet0.png",
+		"images/destroy-sheet0.png",
+		"images/spawn-sheet0.png",
+		"images/spawn-sheet1.png",
+		"images/spawn-sheet2.png",
+		"images/sprite4-sheet0.png",
+		"images/finish-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/9patch.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
